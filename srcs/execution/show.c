3e81b3f78228fa9397c69faac5552067d1a638d8
Fiona LEITZ <fleitz@e2r5p20.clusters.42paris.fr>
2022-10-14 15:19:18 +0200

    il m'a demande de push je sais pas pourquoi

--- srcs/execution/show.c
+++ srcs/execution/show.c
@@ -14,27 +14,28 @@
 
 void	wall_front(t_gbl *gbl)
 {
-	int	x_img_first;
-	int	y_img_first;
-	int	x_img_last;
-	int	y_img_last;
-	int	x_count = -1;
-	int	y_count = -1;
-	char	*pixel;
-	char	*pixel2;
-	int	color;
+	int	x_img_first;			// a gauche du mur
+	int	y_img_first;			// en haut du mur
+	int	x_img_last;				// a droite du mur
+	int	y_img_last;				// en bas du mur
+	int	x_count = -1;			// x sur la texture
+	int	y_count = -1;			// y sur la texture
+	char	*pixel;				// emplacement du pixel qu'on veut dans la textur
+	char	*pixel2;			// emplacement du prochain pixel a poser dans la nouvelle image
+	int	color;					// save de la couleur du pixel de texture
+	double ratio = gbl->vector / 2;
 
-	x_img_first = ((WIDTH_MAX / 2) - (gbl->file.texture.north.width / 2) / (gbl->vector)) - 1;
-	x_img_last = (int)((WIDTH_MAX / 2) + (gbl->file.texture.north.width / 2) / gbl->vector);
+	x_img_first = ((WIDTH_MAX / 2) - (gbl->file.texture.north.width / 2) / (ratio)) - 1;
+	x_img_last = (int)((WIDTH_MAX / 2) + (gbl->file.texture.north.width / 2) / ratio);
 	while (++x_img_first < x_img_last)
 	{
-		y_img_first = ((HEIGHT_MAX / 2) - (gbl->file.texture.north.height / 2) / gbl->vector) - 1;
-		y_img_last = (int)((HEIGHT_MAX / 2) + (gbl->file.texture.north.height / 2) / gbl->vector);
+		y_img_first = ((HEIGHT_MAX / 2) - (gbl->file.texture.north.height / 2) / ratio) - 1;
+		y_img_last = (int)((HEIGHT_MAX / 2) + (gbl->file.texture.north.height / 2) / ratio);
 		y_count = -1;
 		x_count++;
 		while (++y_img_first < y_img_last)
 		{
-			pixel = gbl->file.texture.north.str + ((int)(++y_count * gbl->vector) * gbl->file.texture.north.size_line + (int)(x_count * gbl->vector) * (gbl->file.texture.north.bpp / 8));
+			pixel = gbl->file.texture.north.str + ((int)(++y_count * ratio) * gbl->file.texture.north.size_line + (int)(x_count * ratio) * (gbl->file.texture.north.bpp / 8));
 			color = *(int *)pixel;
 			pixel2 = gbl->new_img.str + (y_img_first * gbl->new_img.size_line + x_img_first * (gbl->new_img.bpp / 8));
 			*(int *)pixel2 = color;
